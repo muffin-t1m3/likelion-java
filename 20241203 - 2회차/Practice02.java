ef87c33469c4b41a93251bf6f88639067240ff8f
muffin-t1m3 <188962149+muffin-t1m3@users.noreply.github.com>
2024-12-03 13:53:13 +0900

    Add Practice02.java

---
+++ 20241203 - 2회차/Practice02.java
@@ -0,0 +1,15 @@
+public class Practice02 {
+    public static void main(String[] args) {
+        int value = 18;
+
+        if (value % 3 == 0)
+            System.out.println("18 mod 3 is 0");
+
+        System.out.println("--------------");
+
+        if (value % 3 == 0) {
+            System.out.println("18 mod 3 is 0");
+            System.out.println("18 mod 3 is 0");
+        }
+    }
+}
