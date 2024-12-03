003fc91d59ce655d56227cf91e94a949a0837426
muffin-t1m3 <188962149+muffin-t1m3@users.noreply.github.com>
2024-12-03 13:51:19 +0900

    Add Practice01.java

---
+++ 20241203 - 2회차/Practice01.java
@@ -0,0 +1,10 @@
+public class Practice01 {
+    public static void main(String[] args) {
+        int value = 0;
+
+        while (value <= 100) {
+            System.out.println(value);
+            value++;
+        }
+    }
+}
