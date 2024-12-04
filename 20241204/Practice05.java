8377e9659d0c0cb2444f6a95dd8342760175c17b
muffin-t1m3 <188962149+muffin-t1m3@users.noreply.github.com>
2024-12-04 15:53:06 +0900

    Add more practice code

---
+++ 20241204/Practice05.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Practice05 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int value = random.nextInt(100);
+        System.out.println(value);
+    }
+}
+
+
