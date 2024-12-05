dcecbb0323e394fc6b250b9bd2b08f3f7eca7e8e
muffin-t1m3 <188962149+muffin-t1m3@users.noreply.github.com>
2024-12-05 16:42:47 +0900

    Add more practice code

---
+++ 20241205/Practice01.java
@@ -0,0 +1,23 @@
+public class Practice01 {
+    private String modelNumber;
+
+    public String getSystemInfo() {
+        return modelNumber;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
---
+++ 20241205/Practice02.java
@@ -0,0 +1,11 @@
+public class Practice02 {
+    public static void main(String[] args) {
+        // 0 <= a< 1
+        double a = Math.random();
+        System.out.println(a);
+
+        // 0 <= b <= 45
+        int b = (int) (Math.random() * 45) + 1;
+        System.out.println(b);
+    }
+}
